4e6d76a954562bd30c59ef0953461c64d5804611
4ware <kai@4ware.net>
2018-05-28 10:23:11 +0200

    added faker and deleting fake user on logout

--- src/modules/logout/logout.test.ts
+++ src/modules/logout/logout.test.ts
@@ -1,12 +1,12 @@
-import axios from "axios";
 import { createTypeormConn } from "../../utils/createTypeormConn";
 import { User } from "../../entity/User";
 import { Connection } from "typeorm";
 import { TestClient } from "../../utils/TestClient";
+import * as faker from 'faker';
 
 let conn: Connection;
-const email = "bob5@bob.com";
-const password = "jlkajoioiqwe";
+const email = faker.internet.email();
+const password = faker.internet.password();
 
 let userId: string;
 beforeAll(async () => {
@@ -20,6 +20,7 @@ beforeAll(async () => {
 });
 
 afterAll(async () => {
+  User.delete({ email });
   conn.close();
 });
 
